7eedad90056047ff02c5081dc388d3ed6609ea5f
Romartiny <romortiny@gmail.com>
2023-06-26 00:19:07 +0300

    fixes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -22,7 +22,7 @@ export const Home = () => {
         <Categories />
         <Sort />
       </div>
-      <h2 className="content__title">Все пиццы</h2>
+      <h2 className="content__title">Menu</h2>
       <div className="content__items">
         {
           isLoading ? [...new Array(6)].map((item, index) => <Skeleton key={index}/>)
