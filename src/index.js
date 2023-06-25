926c0d3dda82234c5ff91ee1ad8e76c31e0ba990
Romartiny <romortiny@gmail.com>
2023-06-25 21:16:40 +0300

    init pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  // <BrowserRouter>
+  //   <Routes>
+  //     <Route path='/' element={<App/>}>
+  //     <Route path='/cart' element={<App/>}>
+  //   </Routes>
+  // </BrowserRouter>
+  <App/>
+);
