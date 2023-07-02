2bcfe2d020c4acb48e975c0e867f18d0f74e8a85
Romartiny <romortiny@gmail.com>
2023-07-02 15:57:44 +0300

    create create and use context to search

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React, {useState} from "react";
+import React, {createContext, useState} from "react";
 
 import Header from "./components/Header";
-
-import "./scss/app.scss";
 import { Home } from "./Pages/Home";
 import {Route, Routes} from "react-router-dom";
 import NotFound from "./Pages/NotFound";
 import Cart from "./Pages/Cart";
+
+import "./scss/app.scss";
+
+export const SearchContext = createContext();
 
 function App() {
   const [searchValue, setSearchValue] = useState('');
 
   return (
     <div className="wrapper">
-      <Header searchValue={searchValue} setSearchValue={ setSearchValue } />
-      <div className="content">
+      <SearchContext.Provider value={{ searchValue, setSearchValue }}>
+        <Header />
+        <div className="content">
           <Routes>
             <Route path="/" element={<Home searchValue={ searchValue }/>} />
             <Route path="/cart" element={<Cart />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
-      </div>
+        </div>
+      </SearchContext.Provider>
     </div>
   );
 }
